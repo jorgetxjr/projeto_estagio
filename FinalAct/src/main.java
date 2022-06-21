4ee7dd15a5f5f6bbc001e5617d10f01f0436a846
Jorge Teixeira II <jorgetxjr@gmail.com>
2022-06-21 20:58:54 -0300

    criadas as funções 1, 3 e 4 superados alguns problemas de entendimento quanto a percorrer uma lista e editá-la ao mesmo tempo

--- FinalAct/src/main.java
+++ FinalAct/src/main.java
@@ -3,11 +3,41 @@ import java.io.File;
 import org.apache.pdfbox.pdmodel.PDDocument;
 //import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.text.PDFTextStripper;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class main 
 {
 	public static void main(String [] args)throws IOException
 	{
+		/*String caminho="";
+		Scanner entrada=new Scanner(System.in);
+		String textoUnisinos="";
+		String textoExterno="";
 		
+		//por isso em um try/catch
+		System.out.print("Entre com o caminho do arquivo da Unisinos: ");
+		caminho=entrada.nextLine();
+		textoUnisinos=adquirirTexto(caminho);
+		//verificar a possibilidade destas informações serem "fixas"
+		entrada.reset();
+		System.out.print("Entre com o caminho do arquivo da universidade externa: ");
+		caminho=entrada.nextLine();
+		textoExterno=adquirirTexto(caminho);
+		*/
+		//String cobaia="a vida é o que é!";
+		String cobaia="Onde a dona Rosa roubou pão? Ela, que é muito esperta, roubou pão na casa do João.";
+		//cobaia=cobaia.toLowerCase();
+		//cobaia=removerPontuacao(cobaia);
+		/* está ocorrendo uma exception que ainda não entendi: java.util.ConcurrentModificationException
+		 * com a função "removerStopWords()
+		 * pondo direto no main, para ver se roda
+		 */
+		//ArrayList<String> recebido0=tokenizarTexto(cobaia);
+		ArrayList<String> recebido=normalizarTexto(cobaia);
+		for(String r:recebido)
+			System.out.print(r+" ");
 	}
 	
 	//-------------------FUNÇÕES SECUNDÁRIAS
@@ -26,9 +56,8 @@ public class main
 	
 	public static String removerPontuacao(String texto)
 	{//função 3 do diagrama
-		
 		String textoRetorno="";
-		final char[]stopPoints= {'.',',','(',')',':',';','\n','\t','/'};
+		final char[]stopPoints= {'.',',','(',')',':',';','\n','\t','/','!','?'};//more?
 		char[] textoEmCharArray=texto.toCharArray();
 		for(char ponto:stopPoints)
 		{
@@ -36,7 +65,7 @@ public class main
 			{
 				if(ponto==textoEmCharArray[i]) 
 				{
-					textoEmCharArray[i]='X';//mudar para algo melhor?
+					textoEmCharArray[i]=' ';//mudar o X para algo melhor?
 				}
 			}
 		}
@@ -46,14 +75,46 @@ public class main
 		return textoRetorno;
 	}
 	
-	public static String removerStopWords(String texto)//testar
-	{//função 4 do diagrama
-		String textoModificado="";
-		final String[] stopWords= {"a","as","o","os","um","uma","uns","umas", "e"};
-		for(String sw:stopWords)
-			textoModificado=texto.replaceAll(sw, "");
-		
+	public static ArrayList<String> tokenizarTexto(String texto)//testar
+	{//função 4, parte 1 do diagrama
+		String[] textoTemp=texto.split("\\s");
+		ArrayList<String> textoModificado=new ArrayList<>();
+		for(String s:textoTemp)
+			textoModificado.add(s);
+						
 		return textoModificado;
+	}
+	
+	public static ArrayList<String> removerStopWords(ArrayList<String> texto)
+	{//função 4, parte 2 do diagrama
+		ArrayList <String> cloneTexto=new ArrayList<>(texto);
+		final String[] stopWords= {"a","as","o","os","um","uma","uns","umas", "e","até", "que", "é"};//more?
+		for(String sw:stopWords)
+		{
+			for(String tm:cloneTexto)
+			{
+				if(sw.equalsIgnoreCase(tm))
+				{
+					texto.remove(tm);
+				}
+			}
+			
+		}
+		return texto;
+		//problemas com 
+		//Solução:
+		//https://pt.stackoverflow.com/questions/18856/itera%c3%a7%c3%a3o-d%c3%a1-erro-de-concurrentmodificationexception-ao-incluir-mais-de-um-bot%c3%a3o
+	}
+	
+	public static ArrayList<String> normalizarTexto(String texto)
+	{
+		texto=texto.toLowerCase();
+		texto=removerPontuacao(texto);
+				
+		ArrayList <String> tokensDoTexto=tokenizarTexto(texto);
+		tokensDoTexto=removerStopWords(tokensDoTexto);
+						
+		return tokensDoTexto;
 	}
 
 }
