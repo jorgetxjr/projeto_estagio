a84d8efe7dcf4620fa6057a69e911c5f17a89d77
Jorge Teixeira II <jorgetxjr@gmail.com>
2022-06-22 18:50:50 -0300

    Feita a função 2 com textos iguais, ela mostra 92% de semelhança. O que é bom para um MVP.

--- FinalAct/src/main.java
+++ FinalAct/src/main.java
@@ -4,8 +4,12 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 //import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.text.PDFTextStripper;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class main 
 {
@@ -26,18 +30,15 @@ public class main
 		caminho=entrada.nextLine();
 		textoExterno=adquirirTexto(caminho);
 		*/
-		//String cobaia="a vida é o que é!";
-		String cobaia="Onde a dona Rosa roubou pão? Ela, que é muito esperta, roubou pão na casa do João.";
-		//cobaia=cobaia.toLowerCase();
-		//cobaia=removerPontuacao(cobaia);
-		/* está ocorrendo uma exception que ainda não entendi: java.util.ConcurrentModificationException
-		 * com a função "removerStopWords()
-		 * pondo direto no main, para ver se roda
-		 */
-		//ArrayList<String> recebido0=tokenizarTexto(cobaia);
-		ArrayList<String> recebido=normalizarTexto(cobaia);
-		for(String r:recebido)
-			System.out.print(r+" ");
+		String cobaia001="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		String cobaia002="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+		ArrayList<String> token001=new ArrayList<>();
+		ArrayList<String> token002=new ArrayList<>();
+		token001=normalizarTexto(cobaia001);
+		token002=normalizarTexto(cobaia002);
+		double percent=compararTextos(token001,token002);
+		
+		System.out.println("Igualdade: "+percent+" %");
 	}
 	
 	//-------------------FUNÇÕES SECUNDÁRIAS
@@ -115,6 +116,36 @@ public class main
 		tokensDoTexto=removerStopWords(tokensDoTexto);
 						
 		return tokensDoTexto;
+	}
+	
+	public static double compararTextos(ArrayList<String> texto1,ArrayList<String> texto2)
+	{//função 2
+		HashMap<String,Integer> todasAsPalavras=new HashMap<String, Integer>();
+		int totalDePalavras=0;
+		//int totalDeIgualdades=0;
+		double percentSemelhanca=-1.0;
+		Set<String> conjTextoComum = new TreeSet<>();
+		//adicionando o primeiro conjunto de palavras
+		for(String palavra:texto1)
+			todasAsPalavras.put(palavra, 1);
+		//adicionando o segundo conjunto de palavras
+		for(String palavra:texto2)
+		{
+			if(todasAsPalavras.containsKey(palavra))
+				todasAsPalavras.put(palavra, todasAsPalavras.get(palavra)+1);
+			else
+				todasAsPalavras.put(palavra, 1);
+				
+		}
+		totalDePalavras=todasAsPalavras.size();
+		for (Map.Entry<String, Integer> w : todasAsPalavras.entrySet()) {
+			if (w.getValue() == 2) {
+				conjTextoComum.add(w.getKey());
+			}
+		}
+		
+		percentSemelhanca=(double)conjTextoComum.size()/totalDePalavras *100;
+		return percentSemelhanca;
 	}
 
 }
